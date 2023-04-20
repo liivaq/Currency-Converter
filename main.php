42481fe226ac1fb32cc83032ae9e4302a555e3fd
Liva Kusina <liiva.kusina@gmail.com>
2023-04-20 19:48:56 +0300

    update

--- main.php
+++ main.php
@@ -1,15 +1,17 @@
-<?php
+<?php declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
+
+use App\CurrencyConverter;
 
 $currency = strtoupper(readline('Enter currency to convert to: '));
 $amount = (float)readline('Enter amount: ');
 
-$converter = new \App\CurrencyConverter();
+$converter = new CurrencyConverter();
 $result = $converter->convertCurrency($amount, $currency);
 
 if ($result) {
-    echo $amount . ' EUR to ' . $currency . ' = ' . $result . PHP_EOL;
+    echo $amount . ' EUR to ' . $currency . ' = ' . number_format($result, 2) . PHP_EOL;
 } else {
     echo 'Sorry, no information found about ' . $currency . PHP_EOL;
 }
